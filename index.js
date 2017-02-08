42145dd3a3190978baec908c9fc355a94ec8d22c
mamaz <hisma.sudradjat@btpn.com>
2017-02-08 15:57:33 +0700

    Finalise api

--- index.js
+++ index.js
@@ -1,31 +1,70 @@
 "use strict"
-
 const express = require('express');
+const ContactManager = require('./contact_manager');
+const contactManager = new ContactManager();
+
+let getErrorObject = function (error) {
+    return {
+        code: error.code || 500,
+        status: "error",
+        message: error.message || error.toString()
+    };
+};
 
 let app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-app.get('/favicon.ico', function(req, res) {
+app.get('/favicon.ico', function (req, res) {
     return res.sendStatus(204);
 });
 
 app.get('/contact/list', (req, res) => {
-
+    try {
+        let contacts = contactManager.getAllContacts();
+        res.json(contacts);
+    } catch (error) {
+        res.json(getErrorObject(error));
+    }
 });
 
-app.put('/contact/', (req, res) => {
-
+app.get('/contact/:username', (req, res) => {
+    try {
+        let username = req.params.username;
+        let contact = contactManager.getContact(username);
+        res.json(contact);
+    } catch (error) {
+        res.json(getErrorObject(error));
+    }
 });
 
-app.post('/contact/list', (req, res) => {
-
+app.put('/contact', (req, res) => {
+    let contactInfo = req.body;
+    try {
+        let contact = contactManager.editContact(contactInfo);
+        res.json(contact);
+    } catch (error) {
+        res.json(getErrorObject(error));
+    }
 });
 
-app.listen(3000, function() {
-	console.log("App listening on port 3000...");
+app.post('/contact', (req, res) => {
+    let contactInfo = req.body;
+    try {
+        let contact = contactManager.createContact(contactInfo);
+        res.json({
+            code: "200",
+            status: "ok"
+        });
+    } catch (error) {
+        res.json(getErrorObject(error));
+    }
+});
+
+app.listen(3000, function () {
+    console.log("App listening on port 3000...");
 });
